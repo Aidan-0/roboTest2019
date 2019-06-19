6eea7195b2663d67daec8c2dfc5800148be54f39
Aidan-0 <Gamesitfo@gmail.com>
2019-06-18 21:20:32 -0500

    First commit

---
+++ src/main/java/frc/robot/subsystems/Pneumatics.java
@@ -0,0 +1,35 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * An example subsystem.  You can replace me with your own Subsystem.
+ */
+public class Pneumatics extends Subsystem {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+
+  public Solenoid sully1;
+
+  public void PistonIn() {
+    sully1.set(true);
+  }
+
+  public void PistonOut() {
+    sully1.set(false);
+  }
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+}
